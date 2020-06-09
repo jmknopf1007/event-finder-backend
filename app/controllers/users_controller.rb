f76f0ad11bc597dd838e512b03130712fd20a7a6
jmknopf1007 <jmknopf1007@gmail.com>
2020-06-09 10:59:58 -0700

    added initial methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+
+    def index
+        user = User.all
+        #puts user
+        render json: user
+        end
+    
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+    
+    def create
+        #byebug
+        user = User.create(user_params)
+        render json: { user: UserSerializer.new(user), jwt: issue_token(user) }, status: :accepted   
+    end
+
+
+
+
+
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:fullname, :username, :bio, :password, :password_confirmation)
+    end   
+
+
 end
