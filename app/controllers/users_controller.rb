278fb02cbd57db550b830b179ce62b35f874ba6b
jmknopf1007 <jmknopf1007@gmail.com>
2020-06-09 11:02:30 -0700

    added update and destroy methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,17 @@ class UsersController < ApplicationController
     end
 
 
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        render :json => user
+    end
 
-
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        render :json => {status: 'deleted'}
+    end
 
 
     private
