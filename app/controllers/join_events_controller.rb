f9a64adcec024c75112603883588ac32d951c3ce
jmknopf1007 <jmknopf1007@gmail.com>
2020-06-16 10:14:20 -0700

    added initial methods to join_events controller

--- app/controllers/join_events_controller.rb
+++ app/controllers/join_events_controller.rb
@@ -1,2 +1,26 @@
 class JoinEventsController < ApplicationController
+
+    def index
+        join_event = JoinEvent.all
+        #puts join_event
+        render json: join_event
+    end
+
+    def show
+        join_event = JoinEvent.find(params[:id])
+        render json: join_event
+    end
+
+
+
+
+
+
+
+    private
+
+    def join_event_params
+        params.require(:join_event).permit(:user_id, :event_id)
+    end
+
 end
