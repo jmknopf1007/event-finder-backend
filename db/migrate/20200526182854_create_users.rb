eeddec4110c50eaf46bf6569f9da110931def512
jmknopf1007 <jmknopf1007@gmail.com>
2020-06-22 14:11:10 -0700

    updated database schema

--- db/migrate/20200526182854_create_users.rb
+++ db/migrate/20200526182854_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :fullname
       t.string :username
+      t.string :prof_picture
       t.text :bio
       t.string :password_digest
 
