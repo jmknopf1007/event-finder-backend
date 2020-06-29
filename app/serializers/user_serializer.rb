a98b941de485d694941434b6af14490a373ec32c
jmknopf1007 <jmknopf1007@gmail.com>
2020-06-29 10:51:37 -0700

    updated seeds file and removed password_digest from user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :fullname, :username, :prof_picture, :bio, :password_digest
+  attributes :id, :fullname, :username, :prof_picture, :bio
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,7 @@ JoinEvent.reset_pk_sequence
 
 u1 = User.create(fullname: "Jacob Knopf", username: "jmknopf", prof_picture: "https://images.pexels.com/photos/3411134/pexels-photo-3411134.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260", bio: "I do what it do.", password: "j")
 u2 = User.create(fullname: "Tia Simmons", username: "tsimmons", prof_picture: "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260", bio: "I love all animals.", password: "t") 
+u3 = User.create(fullname: "Chico Knopf-Simmons", username: "cheetahman", prof_picture: "https://images.pexels.com/photos/573241/pexels-photo-573241.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940", bio: "I love people food!", password: "c")
 
 e1 = Event.create(name: "Birthday Bash", picture: "https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260", description: "We party like it's 1999.", address: "5555 Jacob Road", user_id: 1)
 e2 = Event.create(name: "Christmas Party!", picture: "https://images.pexels.com/photos/717988/pexels-photo-717988.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", description: "It's the most wonderufl time of the year.", address: "2222 Candy Cane Lane", user_id: 2)
