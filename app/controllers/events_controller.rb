14e9d2b3fb8e030284413f4af89976c2a657f6cc
jmknopf1007 <jmknopf1007@gmail.com>
2020-07-12 17:35:22 -0700

    updated events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,20 +13,20 @@ class EventsController < ApplicationController
 
     def create 
         event = Event.create(event_params)
-        render :json => event
+        render json: event
 
     end 
 
     def update
         event = Event.find(params[:id])
         event.update(event_params)
-        render :json => event
+        render json: event
     end 
 
     def destroy
         event = Event.find(params[:id])
         event.destroy
-        render :json => {status: 'deleted'}
+        render json: {status: 'deleted'}
     end
 
     private
